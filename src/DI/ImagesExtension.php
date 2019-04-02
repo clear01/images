1a359ad260e676a0135cfe4c7b4e9fbd44204096
Pavel Jurásek <jurasekpavel@ctyrimedia.cz>
2019-04-02 17:22:00 +0200

    Update compatibility

--- src/DI/ImagesExtension.php
+++ src/DI/ImagesExtension.php
@@ -2,9 +2,8 @@
 
 namespace WebChemistry\Images\DI;
 
+use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Types\Type;
-use Kdyby\Doctrine\Connection;
-use Kdyby\Doctrine\DI\OrmExtension;
 use Nette;
 use WebChemistry\Images\Controls\AdvancedUploadControl;
 use WebChemistry\Images\Controls\UploadControl;
@@ -116,36 +115,35 @@ class ImagesExtension extends Nette\DI\CompilerExtension {
 
 	public function beforeCompile() {
 		$builder = $this->getContainerBuilder();
-        $config = $this->parseConfig();
 
 		$def = $builder->getDefinition('nette.latteFactory');
+		/** @var Nette\DI\ServiceDefinition $def */
 		$def = DIHelper::fixFactoryDefinition($def);
 
 		$def->addSetup(Macros::class . '::install(?->getCompiler())', ['@self'])
 			->addSetup('addProvider', ['imageStorageFacade', $builder->getDefinition($this->prefix('template.facade'))]);
 
-		// kdyby registration
-        if ($config['registerType']){
-            foreach ($builder->findByType(Connection::class) as $name => $_) {
-                /** @var Nette\DI\Definitions\ServiceDefinition $conn */
-                $conn = $builder->getDefinition($name);
-                $conn->addSetup('if (!' . Type::class . '::hasType(?)) { ' . Type::class . '::addType(?, ?); }', [
-                    ImageType::TYPE, ImageType::TYPE, ImageType::class,
-                ]);
-                $conn->addSetup('?->getDatabasePlatform()->registerDoctrineTypeMapping(?, ?)', ['@self', 'db_' . ImageType::TYPE, ImageType::TYPE]);
-            }
-        }
+		// doctrine registration
+		if (class_exists(Connection::class)) {
+			foreach ($builder->findByType(Connection::class) as $name => $_) {
+				/** @var Nette\DI\Definitions\ServiceDefinition $conn */
+				$conn = $builder->getDefinition($name);
+				$conn->addSetup('?->getDatabasePlatform()->registerDoctrineTypeMapping(?, ?)', ['@self', 'db_' . ImageType::TYPE, ImageType::TYPE]);
+			}
+		}
 	}
 
 	public function afterCompile(Nette\PhpGenerator\ClassType $class) {
-		$config = $this->getConfig();
+		$config = (array) $this->getConfig();
 		$init = $class->getMethods()['initialize'];
 
 		if ($config['registerControl']) {
 			$init->addBody(UploadControl::class . '::register();');
 			$init->addBody(AdvancedUploadControl::class . '::register();');
 		}
-
+		if ($config['registerType'] && class_exists(Type::class)) {
+			$init->addBody(Type::class . '::addType(?, ?);', [ImageType::TYPE, ImageType::class]);
+		}
 	}
 
 }
